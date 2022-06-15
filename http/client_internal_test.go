020bc1ace1f78587a03841978473d224b06d7da2
Andres Julian Gismondi <andrees2488@gmail.com>
2022-06-15 10:51:38 -0300

    Add test

--- http/client_internal_test.go
+++ http/client_internal_test.go
@@ -1,8 +1,11 @@
+//go:build integration
+// +build integration
+
 package http_test
 
 import (
 	"context"
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"form3/http"
@@ -14,10 +17,55 @@ func TestClientInternal_Create_success(t *testing.T) {
 		"accounts",
 		"eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
 		http.BaseURL("http://localhost:8080"))
-	res, err := client.Create(context.Background(), []string{"andres"}, "AR")
-	if err != nil {
-		fmt.Printf("error: [%v]", err)
-	}
+	got, err := client.Create(context.Background(), []string{"andres"}, "AR")
 
-	fmt.Printf("Data: [%v]", res)
+	assert.Nil(t, err)
+	assert.NotNil(t, got)
+
+	want := "ad27e265-9605-4b4b-a0e5-3003ea9cc4dc"
+	assert.Equal(t, want, got.ID)
+}
+
+func TestClientInternal_Fetch_success(t *testing.T) {
+	ctx := context.Background()
+	client := http.NewClient(
+		"ad27e265-9605-4b4b-a0e5-3003ea9cc4dc",
+		"accounts",
+		"eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
+		http.BaseURL("http://localhost:8080"))
+
+	wanted, err := client.Create(ctx, []string{"andres"}, "AR")
+	assert.Nil(t, err)
+
+	bankId := wanted.Attributes.BankID
+	got, err := client.Fetch(ctx, bankId)
+	assert.Nil(t, err)
+
+	want := "ad27e265-9605-4b4b-a0e5-3003ea9cc4dc"
+	assert.Equal(t, want, got.ID)
+}
+
+func TestClientInternal_Delete_success(t *testing.T) {
+	ctx := context.Background()
+	client := http.NewClient(
+		"ad27e265-9605-4b4b-a0e5-3003ea9cc4dc",
+		"accounts",
+		"eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
+		http.BaseURL("http://localhost:8080"))
+
+	wanted, err := client.Create(ctx, []string{"andres"}, "AR")
+	assert.Nil(t, err)
+
+	bankId := wanted.Attributes.BankID
+	got, err := client.Fetch(ctx, bankId)
+	assert.Nil(t, err)
+	id := "ad27e265-9605-4b4b-a0e5-3003ea9cc4dc"
+	assert.Equal(t, id, got.ID)
+
+	version := wanted.Version
+	err = client.Delete(ctx, bankId, int(version))
+	assert.Nil(t, err)
+
+	got, err = client.Fetch(ctx, bankId)
+	assert.Contains(t, err.Error(), "not found")
 }
--- http/client_test.go
+++ http/client_test.go
@@ -3,6 +3,7 @@ package http_test
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	http2 "net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,7 +16,7 @@ import (
 	"form3/model"
 )
 
-func TestClient_Create_success(t *testing.T) {
+func TestClient_Create(t *testing.T) {
 	want := model.AccountData{
 		ID:             "111",
 		OrganisationID: "abc-123",
@@ -27,25 +28,181 @@ func TestClient_Create_success(t *testing.T) {
 		},
 	}
 
-	ctx := context.Background()
-	server := httptest.NewServer(http2.HandlerFunc(func(response http2.ResponseWriter, request *http2.Request) {
-		response.WriteHeader(http2.StatusOK)
-
-		json.NewEncoder(response).Encode(want)
-	}))
-	defer server.Close()
-
-	client := http.NewClient("111",
-		"account",
-		"abc-123",
-		http.BaseURL(server.URL),
-		http.Timeout(3*time.Second))
-
-	clientOptions := []http3_go_lib.AccountOption{
-		http3_go_lib.BankID("123-321"),
+	type fields struct {
+		http2.HandlerFunc
 	}
-	got, err := client.Create(ctx, []string{"andres"}, "AR", clientOptions...)
 
-	assert.Nil(t, err)
-	assert.Equal(t, want, got)
+	tests := []struct {
+		name string
+		fields
+		want model.AccountData
+	}{
+		{
+			name: "success",
+			fields: fields{
+				http2.HandlerFunc(func(response http2.ResponseWriter, request *http2.Request) {
+					response.WriteHeader(http2.StatusOK)
+					json.NewEncoder(response).Encode(want)
+				}),
+			},
+			want: want,
+		},
+		{
+			name: "error decoding response",
+			fields: fields{
+				http2.HandlerFunc(func(response http2.ResponseWriter, request *http2.Request) {
+					response.WriteHeader(http2.StatusOK)
+					json.NewEncoder(response).Encode("{")
+				}),
+			},
+			want: model.AccountData{},
+		},
+		{
+			name: "response api error",
+			fields: fields{
+				http2.HandlerFunc(func(response http2.ResponseWriter, request *http2.Request) {
+					response.WriteHeader(http2.StatusBadRequest)
+				}),
+			},
+			want: model.AccountData{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			server := httptest.NewServer(tt.fields.HandlerFunc)
+			defer server.Close()
+
+			client := http.NewClient("111",
+				"account",
+				"abc-123",
+				http.BaseURL(server.URL),
+				http.Timeout(3*time.Second))
+
+			clientOptions := []http3_go_lib.AccountOption{
+				http3_go_lib.BankID("123-321"),
+			}
+			got, _ := client.Create(ctx, []string{"andres"}, "AR", clientOptions...)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestClient_Fetch(t *testing.T) {
+	want := model.AccountData{
+		ID:             "111",
+		OrganisationID: "abc-123",
+		Type:           "account",
+		Attributes: model.AccountAttributes{
+			Name:    []string{"andres"},
+			BankID:  "123-321",
+			Country: "AR",
+		},
+	}
+
+	type fields struct {
+		http2.HandlerFunc
+	}
+
+	tests := []struct {
+		name string
+		fields
+		want model.AccountData
+	}{
+		{
+			name: "success",
+			fields: fields{
+				http2.HandlerFunc(func(response http2.ResponseWriter, request *http2.Request) {
+					response.WriteHeader(http2.StatusOK)
+					json.NewEncoder(response).Encode(want)
+				}),
+			},
+			want: want,
+		},
+		{
+			name: "error decoding response",
+			fields: fields{
+				http2.HandlerFunc(func(response http2.ResponseWriter, request *http2.Request) {
+					response.WriteHeader(http2.StatusOK)
+					json.NewEncoder(response).Encode("{")
+				}),
+			},
+			want: model.AccountData{},
+		},
+		{
+			name: "response api error",
+			fields: fields{
+				http2.HandlerFunc(func(response http2.ResponseWriter, request *http2.Request) {
+					response.WriteHeader(http2.StatusBadRequest)
+				}),
+			},
+			want: model.AccountData{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			server := httptest.NewServer(tt.fields.HandlerFunc)
+			defer server.Close()
+
+			client := http.NewClient("111",
+				"account",
+				"abc-123",
+				http.BaseURL(server.URL),
+				http.Timeout(3*time.Second))
+
+			got, _ := client.Fetch(ctx, "123-321")
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestClient_Delete(t *testing.T) {
+	type fields struct {
+		http2.HandlerFunc
+	}
+
+	tests := []struct {
+		name string
+		fields
+		want error
+	}{
+		{
+			name: "success",
+			fields: fields{
+				http2.HandlerFunc(func(response http2.ResponseWriter, request *http2.Request) {
+					response.WriteHeader(http2.StatusOK)
+				}),
+			},
+			want: nil,
+		},
+		{
+			name: "error deleting account",
+			fields: fields{
+				http2.HandlerFunc(func(response http2.ResponseWriter, request *http2.Request) {
+					response.WriteHeader(http2.StatusBadRequest)
+				}),
+			},
+			want: fmt.Errorf("account api error: %s", "400 Bad Request"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			server := httptest.NewServer(tt.fields.HandlerFunc)
+			defer server.Close()
+
+			client := http.NewClient("111",
+				"account",
+				"abc-123",
+				http.BaseURL(server.URL),
+				http.Timeout(3*time.Second))
+
+			err := client.Delete(ctx, "123-321", 0)
+			assert.Equal(t, tt.want, err)
+		})
+	}
 }
